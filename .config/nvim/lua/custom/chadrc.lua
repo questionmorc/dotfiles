457804afd34ec6f8b691ed520600bd5fc0214200
questionmorc <questionmorc@gmail.com>
2024-02-24 13:25:15 -0500

    feat: show nvchad dashboard on startup"

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,10 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+  theme = 'catppuccin',
+  nvdash = { load_on_startup = true}
+}
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
 return M
