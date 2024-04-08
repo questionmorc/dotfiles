6335ab18b58cb422be7bc7d7de83c1c7b982a730
Marc Alloul <marc.alloul@humanfirst.ai>
2024-04-08 10:26:20 -0400

    new tmux sessiosn for onprem

---
+++ tmux-layouts/onprem.session.sh
@@ -0,0 +1,26 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/code/onprem"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "onprem"; then
+
+  # Create a new window inline within session layout definition.
+  #new_window "Editor"
+
+  # Load a defined window layout.
+  load_window "onprem"
+  #tmux split-window -t "$session:$window.1" -v -p 50
+  
+  load_window "onprem-vm"
+
+  select_window "onprem"
+
+  # Select the default active window on session creation.
+  #select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
