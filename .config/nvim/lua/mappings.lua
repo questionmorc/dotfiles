a047450edc16b4e5a33b4d25437b1ad8672bc2ed
Marc Alloul <malloul@turbulent.ca>
2024-04-08 20:29:01 -0400

    reorg

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -59,3 +59,15 @@ map({ "n" }, "<leader>mN", "<cmd>lua require('harpoon'):list():prev()<cr>", { de
 --     ["<leader>u"] = { vim.cmd.UndotreeToggle },
 --   }
 -- }
+--
+local diagnostics_active = true
+local toggle_diagnostics = function()
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+      vim.diagnostic.show()
+  else
+    vim.diagnostic.hide()
+  end
+end
+
+vim.keymap.set('n', '<leader>tt', toggle_diagnostics)
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,21 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--local opt = vim.opt
+
+local opt = vim.opt
+
+opt.guicursor = ""
+opt.relativenumber = true
+
+
+opt.hlsearch = false
+opt.incsearch = true
+opt.scrolloff = 8
+
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
