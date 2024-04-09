0bd50bfcdd5dacc466bc88d01b728e82848199ee
Marc Alloul <malloul@turbulent.ca>
2024-04-08 20:24:32 -0400

    start seperating config into own files

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,38 +1,95 @@
-return {
+local plugins = {
   {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
+    lazy = false,
+    "github/copilot.vim"
+  },
+  {
+    "towolf/vim-helm",
+    ft = "helm"
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = function()
+      require "nvchad.configs.treesitter"
+      require "configs.treesitter"
     end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  --  {
+  --    "mbbill/undotree",
+  --    lazy = false,
+  --    init = function()
+  --      require("core.utils").load_mappings("undotree")
+  --    end
+  --  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "terraform-ls",
+        "golangci-lint",
+        "golangci-lint-langserver",
+        "jsonnet-language-server",
+        "lua-language-server ",
+        "jsonnetfmt",
+        "helm-ls",
+        "llm-ls",
+        "circleci-yaml-language-server",
+        "bash-language-server",
+
+      }
+    }
+  },
+  --  {
+  --    "mfussenegger/nvim-dap",
+  --    init = function()
+  --      require("core.utils").load_mappings("dap")
+  --    end
+  --  },
+  --  {
+  --    "dreamsofcode-io/nvim-dap-go",
+  --    ft = "go",
+  --    dependencies = "mfussenegger/nvim-dap",
+  --    config = function(_, opts)
+  --      require("dap-go").setup(opts)
+  --      require("core.utils").load_mappings("dap_go")
+  --    end
+  --  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    ft = { "go", "terraform", "tf", "terraform-vars", "hcl" },
+    dependencies = "nvim-lua/plenary.nvim",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+  --  {
+  --    "olexsmir/gopher.nvim",
+  --    ft = "go",
+  --    config = function(_, opts)
+  --      require("gopher").setup(opts)
+  --      require("core.utils").load_mappings("gopher")
+  --    end,
+  --    build = function()
+  --      vim.cmd [[silent! GoInstallDeps]]
+  --    end,
+  --  },
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
 }
+return plugins
