19b88e64f3e607348f819d78fed9174e46499bf7
Marc Alloul <malloul@turbulent.ca>
2024-04-08 22:19:19 -0400

    cleanup

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,7 +1,4 @@
 require "nvchad.options"
-
--- add yours here!
-
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 --local opt = vim.opt
@@ -11,14 +8,11 @@ local opt = vim.opt
 opt.guicursor = ""
 opt.relativenumber = true
 
-
 opt.hlsearch = false
 opt.incsearch = true
 opt.scrolloff = 8
-
 
 opt.swapfile = false
 opt.backup = false
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
-
