a801977db21ab0fcb62da3115a11445ffd507679
Marc Alloul <marc.alloul@humanfirst.ai>
2024-05-29 13:41:01 -0400

    add formatting

--- .config/nvim/lua/mrrc/lazy/conform.lua
+++ .config/nvim/lua/mrrc/lazy/conform.lua
@@ -1,11 +1,44 @@
 return {
     "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-        },
-    },
-    config = function(_, opts)
-        require("conform").setup(opts)
-    end,
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = { "ConformInfo", },
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                go = { "gofmt", "gofumpt" },
+                jsonnnet = { "jsonnetfmt" },
+                terraform = { "terraform_fmt" },
+                svelte = { { "prettierd", "prettier" } },
+                javascript = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+                javascriptreact = { { "prettierd", "prettier" } },
+                typescriptreact = { { "prettierd", "prettier" } },
+                json = { { "prettierd", "prettier" } },
+                graphql = { { "prettierd", "prettier" } },
+                java = { "google-java-format" },
+                ruby = { "standardrb" },
+                markdown = { { "prettierd", "prettier" } },
+                erb = { "htmlbeautifier" },
+                html = { "htmlbeautifier" },
+                bash = { "beautysh" },
+                sh = { "beautysh" },
+                proto = { "buf" },
+                rust = { "rustfmt" },
+                yaml = { "yamlfix" },
+                toml = { "taplo" },
+                css = { { "prettierd", "prettier" } },
+                scss = { { "prettierd", "prettier" } },
+                ansible = { "ansible_lint" },
+            },
+        })
+        vim.keymap.set({ "n", "v" }, "<leader>l", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end
 }
