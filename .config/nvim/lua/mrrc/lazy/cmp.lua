2ff1f08ae705c0632ffe9584a902e8d7f397ebb2
Marc Alloul <marc.alloul@humanfirst.ai>
2024-05-30 12:45:41 -0400

    add cmd completions

--- .config/nvim/lua/mrrc/lazy/cmp.lua
+++ .config/nvim/lua/mrrc/lazy/cmp.lua
@@ -8,6 +8,7 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
+            -- "hrsh7th/cmp-copilot",
             "hrsh7th/cmp-buffer",
             "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
@@ -15,7 +16,30 @@ return {
         config = function()
             local cmp = require('cmp')
             local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            local lspkind = require "lspkind"
+            lspkind.init {}
+            -- `/` cmdline setup.
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
 
+            -- `:` cmdline setup.
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    {
+                        name = "cmdline",
+                        option = {
+                            ignore_cmds = { "Man", "!" },
+                        },
+                    },
+                }),
+            })
             cmp.setup({
                 snippet = {
                     expand = function(args)
@@ -26,26 +50,18 @@ return {
                     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.complete(),
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
-                    { name = 'luasnip' }, -- For luasnip users.
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    -- { name = 'copilot' },
+                    -- { name = 'cmdline' },
+                    -- { name = 'buffer' },
                 }, {
                     { name = 'buffer' },
                 })
-            })
-
-            vim.diagnostic.config({
-                -- update_in_insert = true,
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                },
             })
         end
     }
