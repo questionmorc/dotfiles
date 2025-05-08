65f4b60a2126fcd63e5c41d94197f514e4f03e33
questionmorc <questionmorc@gmail.com>
2025-05-08 00:05:58 -0400

    snacks

---
+++ .config/nvim/lua/mrrc/plugins/snacks.lua
@@ -0,0 +1,50 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- Disable all snacks by default
+    -- Only explicitly enabled snacks will be active
+    animate = { enabled = false },
+    bigfile = { enabled = false },
+    bufdelete = { enabled = false },
+    dashboard = { enabled = false },
+    debug = { enabled = false },
+    dim = { enabled = false },
+    explorer = { enabled = false },
+    git = { enabled = false },
+    gitbrowse = { enabled = true },
+    image = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    layout = { enabled = false },
+    lazygit = { enabled = false },
+    notifier = { enabled = false },
+    notify = { enabled = false },
+    picker = { enabled = false },
+    profiler = { enabled = false },
+    quickfile = { enabled = false },
+    rename = { enabled = false },
+    scope = { enabled = false },
+    scratch = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    terminal = { enabled = false },
+    toggle = { enabled = false },
+    util = { enabled = false },
+    win = { enabled = false },
+    words = { enabled = false },
+    zen = { enabled = true },
+  },
+  keys = {
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    -- { "<leader>bd", function() Snacks.bufdelete() end,       desc = "Buffer delete",       mode = "n" },
+    -- { "<leader>ba", function() Snacks.bufdelete.all() end,   desc = "Buffer delete all",   mode = "n" },
+    -- { "<leader>bo", function() Snacks.bufdelete.other() end, desc = "Buffer delete other", mode = "n" },
+    { "<leader>zz", function() Snacks.zen() end,             desc = "Toggle Zen Mode",     mode = "n" },
+  },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+  end
+}
