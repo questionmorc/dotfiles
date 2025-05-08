1d0c49648b244b168767c2b96060d671f933a252
questionmorc <questionmorc@gmail.com>
2025-05-07 23:20:18 -0400

    snips

--- .config/nvim/lua/mrrc/plugins/blink-cmp.lua
+++ .config/nvim/lua/mrrc/plugins/blink-cmp.lua
@@ -1,16 +1,34 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    -- dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = {
+      'L3MON4D3/LuaSnip', version = 'v2.*'
+    },
 
     version = 'v0.*',
 
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'default',
+        ['<C-h>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      },
+      snippets = { preset = 'luasnip' },
 
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
+      },
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
       },
 
       signature = { enabled = true },
