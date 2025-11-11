fd8085929680036426e5282c0d3f7b184879cb11
questionmorc <questionmorc@gmail.com>
2025-11-10 22:05:33 -0500

    use bundled

--- .config/nvim/lua/mrrc/plugins/mcphub.lua
+++ .config/nvim/lua/mrrc/plugins/mcphub.lua
@@ -4,8 +4,11 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+  -- build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+  build = "bundled_build.lua",     -- Bundles `mcp-hub` binary along with the neovim plugin
   config = function()
-    require("mcphub").setup()
+    require("mcphub").setup({
+      use_bundled_binary = true
+    })
   end
 }
